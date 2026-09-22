e7181f67f5b2f315d71a270ed42682286c97b3fc
fardeenpathan <md.fardeen5236@gmail.com>
2026-09-22 14:50:09 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,11 @@
-"use client"
+"use client";
 import Image from "next/image";
 import IntroVideo from "./components/IntroVideo";
 import Countdown from "./components/Countdown";
 import Events from "./components/Events";
 import { useEffect, useState, useRef } from "react";
 export default function Home() {
-
-
-
- const audioRef = useRef(null);
+  const audioRef = useRef(null);
   const [started, setStarted] = useState(false);
   const [playing, setPlaying] = useState(false);
 
@@ -21,7 +18,7 @@ export default function Home() {
       await audio.play();
       setStarted(true);
       setPlaying(true);
-    } catch { }
+    } catch {}
   };
 
   const toggleMusic = async () => {
@@ -35,7 +32,7 @@ export default function Home() {
       try {
         await audio.play();
         setPlaying(true);
-      } catch { }
+      } catch {}
     }
   };
 
@@ -52,12 +49,11 @@ export default function Home() {
     };
   }, [started]);
 
-
   return (
     <>
-    <IntroVideo />
+      <IntroVideo />
 
-<button
+      <button
         onClick={() => {
           started ? toggleMusic() : startMusic();
         }}
@@ -66,55 +62,59 @@ export default function Home() {
         {playing ? "⏸" : "▶"}
       </button>
 
-      <audio ref={audioRef} src="/assets/laavan.mp3" loop preload="auto" playsInline />
-<div className="relative w-full h-screen overflow-hidden flex items-center justify-center">
-  <video
-    autoPlay
-    muted
-    loop
-    playsInline
-    preload="auto"
-    className="absolute inset-0 w-full h-full object-cover opacity-50"
-  >
-    <source src="/assets/first_section.mp4" type="video/mp4" />
-  </video>
+      <audio
+        ref={audioRef}
+        src="/assets/laavan.mp3"
+        loop
+        preload="auto"
+        playsInline
+      />
+      <div className="relative w-full h-screen overflow-hidden flex items-center justify-center">
+        <video
+          autoPlay
+          muted
+          loop
+          playsInline
+          preload="auto"
+          className="absolute inset-0 w-full h-full object-cover opacity-50"
+        >
+          <source src="/assets/first_section.mp4" type="video/mp4" />
+        </video>
 
-  {/* Content */}
-  <div className="relative z-10 flex flex-col items-center text-center px-4">
+        {/* Content */}
+        <div className="relative z-10 flex flex-col items-center text-center px-4">
+          <img
+            src="/assets/ganesh_img.png"
+            alt="Ganesh"
+            className="md:h-35 md:w-35 h-23 w-25 object-contain"
+          />
+          <h2 className="text-white md:text-2xl text-[14px] font-cormorant-garamond uppercase mt-2">
+            || ॐ श्री गणेशाय नम: ||
+          </h2>
+          <h2 className="text-white md:text-3xl text-xl font-cormorant-garamond uppercase mt-20">
+            Celebrating The Union Of
+          </h2>
 
-    <img
-      src="/assets/ganesh_img.png"
-      alt="Ganesh"
-      className="md:h-35 md:w-35 h-23 w-25 object-contain"
-    />
-<h2 className="text-white md:text-2xl text-[14px] font-cormorant-garamond uppercase mt-2">
-     || ॐ श्री गणेशाय नम: ||
-    </h2>
-    <h2 className="text-white md:text-3xl text-xl font-cormorant-garamond uppercase mt-20">
-      Celebrating The Union Of
-    </h2>
+          <h1 className="text-white md:text-7xl text-5xl font-dancing-script mt-12">
+            Muskaan Saini
+          </h1>
 
-    <h1 className="text-white md:text-7xl text-5xl font-dancing-script mt-12">
-      Muskaan Saini
-    </h1>
+          <h2 className="text-white md:text-5xl text-3xl font-cormorant-garamond my-4">
+            Weds
+          </h2>
 
-    <h2 className="text-white md:text-5xl text-3xl font-cormorant-garamond my-4">
-      Weds
-    </h2>
+          <h1 className="text-white md:text-7xl text-5xl font-dancing-script">
+            Manjinder Singh
+          </h1>
 
-    <h1 className="text-white md:text-7xl text-5xl font-dancing-script">
-      Manjinder Singh
-    </h1>
+          <h2 className="text-white md:text-3xl text-xl font-cormorant-garamond uppercase mt-20">
+            19th November 2026
+          </h2>
+        </div>
+      </div>
 
-    <h2 className="text-white md:text-3xl text-xl font-cormorant-garamond uppercase mt-20">
-      19th November 2026
-    </h2>
-
-  </div>
-</div>
-
-<Countdown/>
-<Events/>
+      <Countdown />
+      <Events />
     </>
   );
 }
